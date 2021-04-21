596c93ad62fcab186f43881ac7078c7b230e751b
shrikant chine <shrikant.chine@gmail.com>
2021-04-21 19:56:13 +0200

    Adding linked list class

---
+++ src/singly_linked_list.py
@@ -0,0 +1,12 @@
+class ListNode:
+    def __init__(self, data, nxt=None):
+        self.data = data
+        self.next = nxt
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_end(self, data):
+        pass
